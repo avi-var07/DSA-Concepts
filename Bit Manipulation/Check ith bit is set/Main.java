fad6f91601364045e9bae32e8d7bcec36acd2fda
Aviral Varshney <aviralvarshney07@gmail.com>
2025-04-24 19:17:21 +0530

    Check ith bit is set by both left and right shift

--- Bit Manipulation/Check ith bit is set/Main.java
+++ Bit Manipulation/Check ith bit is set/Main.java
@@ -1,8 +1,11 @@
 import java.util.*;
 
 class Main {
-    public static boolean check(int n, int i){
+    public static boolean checkByLeftShift(int n, int i){
         return (n&(1<<i))!=0;
+    }
+    public static boolean checkByRightShift(int n, int i){
+        return ((n>>i)&1)!=0;
     }
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
@@ -10,8 +13,10 @@ class Main {
         int n = sc.nextInt();
         System.out.println("Enter the bit position to check: ");
         int i = sc.nextInt();
-        if(check(n, i))System.out.println("Bit is set");
+        if(checkByLeftShift(n, i))System.out.println("Bit is set");
         else System.out.println("Bit is not set");
+        if(checkByRightShift(n, i))System.out.println("True");
+        else System.out.println("False");
         sc.close();
     }
 }
