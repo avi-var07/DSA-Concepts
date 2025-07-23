d0dbcaeb6fc741f28a71cd505b75ece3e1c3c5b6
Aviral Varshney <aviralvarshney07@gmail.com>
2025-07-23 22:22:24 +0530

    Count Number of Subarrays with XOR k

---
+++ Subarrays/Count the number of subarrays with given xor K/Solution.java
@@ -0,0 +1,50 @@
+/* Approach:
+The steps are as follows:
+
+First, we will declare a map to store the prefix XORs and their counts.
+Then, we will set the value of 0 as 1 on the map.
+Then we will run a loop(say i) from index 0 to n-1(n = size of the array).
+For each index i, we will do the following:
+We will XOR the current element i.e. arr[i] to the existing prefix XOR.
+Then we will calculate the prefix XOR i.e. xr^k, for which we need the occurrence.
+We will add the occurrence of the prefix XOR xr^k i.e. mpp[xr^k] to our answer.
+Then we will store the current prefix XOR, xr in the map increasing its occurrence by 1.
+Question: Why do we need to set the value of 0 beforehand?
+Let’s understand this using an example. Assume the given array is [3, 3, 1, 1, 1] and k is 3. Now, for index 0, we get the total prefix XOR as 3, and k is also 3. So, the prefix XOR xr^k will be = 3^3 = 0. Now, if the value is not previously set for the key 0 in the map, we will get the default value 0 and we will add 0 to our answer. This will mean that we have not found any subarray with XOR 3 till now. But this should not be the case as index 0 itself is a subarray with XOR k i.e. 3.
+So, in order to avoid this situation we need to set the value of 0 as 1 on the map beforehand.*/
+
+import java.util.*;
+
+class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Enter " + n + " elements:");
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++) 
+        arr[i] = sc.nextInt();
+        
+        System.out.println("Enter the value of k: ");
+        int k = sc.nextInt();
+
+        System.out.println("Number of subarrays with xor k is: "+ subarraysWithXorK(arr, k));
+        sc.close();
+    }
+    static int subarraysWithXorK(int arr[], int k){
+        int count =0;
+        int xorr = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(xorr,1);
+        for(int i=0;i<arr.length;i++){
+            xorr^=arr[i];
+
+            int x = xorr^k; //as per formula
+
+            if(map.containsKey(x))count+=map.get(x);
+
+            map.put(xorr, map.getOrDefault(xorr,0 )+1);
+        }
+        return count;
+    }
+}
