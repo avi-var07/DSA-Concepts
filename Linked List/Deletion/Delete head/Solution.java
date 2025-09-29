4148e67658a281bfaecb40764895bc0ad3385c7e
Aviral Varshney <aviralvarshney07@gmail.com>
2025-09-29 19:07:09 +0530

    Delete the headn of LL

---
+++ Linked List/Deletion/Delete head/Solution.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data=data;
+        this.next = null;
+    }
+}
+
+class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Enter " + n + " elements:");
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++) 
+        arr[i] = sc.nextInt();
+        
+        Node head = convertToLinkedList(arr);
+
+        print(head);
+        System.out.println();
+
+        head = deleteHead(head);
+        print(head);
+        
+        sc.close();
+    }
+    static Node convertToLinkedList(int arr[]){
+        Node head = new Node(arr[0]);
+
+        Node current = head;
+        for(int i=1;i<arr.length;i++){
+            Node temp = new Node(arr[i]);
+            current.next=temp;
+            current = temp;
+        }
+        return head;
+    }
+    static void print(Node head){
+        Node temp  = head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+    }
+    static Node deleteHead(Node head){
+        
+        if(head==null)return head;
+
+        head = head.next;
+
+        return head;
+    }
+}
