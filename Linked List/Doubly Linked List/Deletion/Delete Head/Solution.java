9d7faacb9fd2a733de85ecf672012f909a8b22d4
Aviral Varshney <aviralvarshney07@gmail.com>
2025-10-07 23:53:03 +0530

    Delete head from doubly linked list

---
+++ Linked List/Doubly Linked List/Deletion/Delete Head/Solution.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class Node{
+    int data;
+    Node next;
+    Node back;
+
+    Node(int data){
+        this.data=data;
+        this.next = null;
+        this.back = null;
+    }
+}
+
+class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Enter " + n + " elements:");
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++) 
+        arr[i] = sc.nextInt();
+        
+        Node head = convertToLinkedList(arr);
+
+        print(head);
+        System.out.println();
+
+        head = deleteHead(head);
+        print(head);
+        
+        sc.close();
+    }
+    static Node convertToLinkedList(int arr[]){
+        Node head = new Node(arr[0]);
+        Node prev = head;
+
+        for(int i=1;i<arr.length;i++){
+            Node temp = new Node(arr[i]);
+            temp.back = prev;
+            prev.next = temp;
+            prev=temp;
+        }
+        return head;
+    }
+    static void print(Node head){
+        Node temp  = head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+    }
+    static Node deleteHead(Node head){
+        
+        if(head==null||head.next==null)return null;
+
+        Node prev = head;
+        head=head.next;
+        head.back = null;
+        prev.next=null;
+
+        return head;
+
+    }
+}
