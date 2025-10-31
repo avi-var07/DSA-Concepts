a32e2fd95d4f73cb94f2046d9a8bb81d42d6ddc5
Aviral Varshney <aviralvarshney07@gmail.com>
2025-10-31 23:16:03 +0530

    Stack Implementation using Array

--- Stack and Queue/Implement Queue using Array/Solution.java
+++ Stack and Queue/Implement Queue using Array/Solution.java
@@ -2,53 +2,61 @@ import java.util.*;
 
 class Solution {
 
-    static int front = -1, rear = -1, size = 0;
-    static int[] q;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter capacity of queue: ");
         int n = sc.nextInt();
-        q = new int[n];
+        int q[] = new int[n];
 
         System.out.print("Enter number of operations: ");
         int ops = sc.nextInt();
-        sc.nextLine(); // consume newline
-
-        System.out.println("Available operations: enqueue, dequeue, peek, size");
+        sc.nextLine(); 
+        int start = -1, end=-1, currSize = 0;
         for (int i = 0; i < ops; i++) {
+            System.out.println("Available operations: enqueue, dequeue, peek, size");
             System.out.print("Enter operation: ");
             String op = sc.nextLine().toLowerCase();
 
             switch (op) {
                 case "enqueue":
-                    if (rear == n - 1) {
-                        System.out.println("Queue overflow!");
+                    if(currSize==n){
+                        System.out.println("Overflow");
                         break;
                     }
+                    if(currSize==0){
+                        start=0;
+                        end=0;
+                    }
+                    else end = (end+1)%n;
                     System.out.print("Enter value to enqueue: ");
                     int val = sc.nextInt();
                     sc.nextLine();
-                    enqueue(val);
+                    q[end]=val;
+                    currSize++;
                     break;
 
                 case "dequeue":
-                    if (front == -1 || front > rear)
-                        System.out.println("Queue underflow!");
-                    else
-                        System.out.println("Dequeued element: " + dequeue());
+                    if (currSize==0){
+                        System.out.println("Queue is empty");
+                        break;
+                    }
+                    else if(currSize==1){
+                        System.out.println("Element dequeued is: "+q[start]);
+                        start=-1;
+                        end=-1;
+                    }
+                    else start = (start+1)%n;
+                    currSize--;
                     break;
 
                 case "peek":
-                    if (front == -1 || front > rear)
-                        System.out.println("Queue is empty!");
-                    else
-                        System.out.println("Front element: " + peek());
+                    if (currSize==0)System.out.println("Queue is empty!");
+                    else System.out.println("Peek element: " + q[start]);
                     break;
 
                 case "size":
-                    System.out.println("Current size: " + size());
+                    System.out.println("Current size: " + currSize);
                     break;
 
                 default:
@@ -60,26 +68,4 @@ class Solution {
         sc.close();
     }
 
-    static void enqueue(int x) {
-        if (front == -1) front = 0;
-        rear++;
-        q[rear] = x;
-        size++;
-        System.out.println(x + " added to queue");
-    }
-
-    static int dequeue() {
-        int val = q[front];
-        front++;
-        size--;
-        return val;
-    }
-
-    static int peek() {
-        return q[front];
-    }
-
-    static int size() {
-        return size;
-    }
 }
