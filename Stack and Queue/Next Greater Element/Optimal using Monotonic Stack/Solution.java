4d42f3ec81509be5bcd9729f47a77c4712642d47
Aviral Varshney <aviralvarshney07@gmail.com>
2025-11-03 23:16:56 +0530

    NGE Optimal

---
+++ Stack and Queue/Next Greater Element/Optimal using Monotonic Stack/Solution.java
@@ -0,0 +1,57 @@
+/*You are given an array arr[] of integers, the task is to find the next greater element for each element of the array in order of their appearance in the array. Next greater element of an element in the array is the nearest element on the right which is greater than the current element.
+If there does not exist next greater of current element, then next greater element for current element is -1.
+
+Examples
+
+Input: arr[] = [1, 3, 2, 4]
+Output: [3, 4, 4, -1]
+Explanation: The next larger element to 1 is 3, 3 is 4, 2 is 4 and for 4, since it doesn't exist, it is -1.
+Input: arr[] = [6, 8, 0, 1, 3]
+Output: [8, -1, 1, 3, -1]
+Explanation: The next larger element to 6 is 8, for 8 there is no larger elements hence it is -1, for 0 it is 1, for 1 it is 3 and then for 3 there is no larger element on right and hence -1.
+Input: arr[] = [1, 2, 3, 5]
+Output: [2, 3, 5, -1]
+Explanation: For a sorted array, the next element is next greater element also except for the last element.
+Input: arr[] = [5, 4, 3, 1]
+Output: [-1, -1, -1, -1]
+Explanation: There is no next greater element for any of the elements in the array, so all are -1.
+Constraints:
+1 ≤ arr.size() ≤ 106
+0 ≤ arr[i] ≤ 109
+
+ */
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of Elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter "+n+" elements: ");
+        for(int i=0;i<n;i++) arr[i] = sc.nextInt();
+
+        ArrayList<Integer>ans = nextLargerElement(arr);
+        for(var ele: ans)System.out.print(ele+" ");
+        
+        sc.close();
+    }
+    public static ArrayList<Integer> nextLargerElement(int[] arr) {
+        // code here
+        ArrayList<Integer>ans = new ArrayList<>();
+        Stack<Integer>st = new Stack<>();
+        int n = arr.length;
+        
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty()&&st.peek()<=arr[i])st.pop();
+            
+            if(st.isEmpty())ans.add(-1);
+            else ans.add(st.peek());
+            
+            st.push(arr[i]);
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
